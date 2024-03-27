32287e1ba538b061afa987b0eb0d2cfd5529a1ec
Achraf <achraf@example.com>
2024-03-27 18:12:02 +0000

    Added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,29 @@
-
-import './App.css'
-import BarecodeGenerator from './Component/BarecodeGenerator'
+import { useEffect, useState } from "react";
+import "./App.css";
+import BarecodeGenerator from "./Component/BarecodeGenerator";
+import Barcode from "react-barcode";
 
 function App() {
-  
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("/Data.json")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        console.log(res);
+      });
+  }, []);
 
   return (
-    <div className='container'>
-      <BarecodeGenerator/>
+    <div className="container">
+      {data &&
+        data.ordinateurs.map((d) => <Barcode value={d.Numéro_de_série} />)}
+      <hr />
+      <h1>imprimantes</h1>
+      {data && data.imprimantes.map((d) => <Barcode value={d.SN} />)}
     </div>
-      )
+  );
 }
 
-export default App
+export default App;
